cc5a4bf458ba7955b0a61b3e360d8514772d45f0
AzurIce <973562770@qq.com>
2025-03-09 16:15:49 +0800

    chore: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "cript";
+  description = "A simple text file encrypt/decrypt cli tool";
 
   # nixConfig = {
   #   extra-substituters = [
@@ -10,47 +10,58 @@
   #   ];
   # };
 
-
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, crane, flake-utils, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
-          inherit system overlays;
+          inherit system;
+          overlays = [ (import rust-overlay) ];
         };
-        rust-tools = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          # prioritize system clang, see https://github.com/zed-industries/zed/issues/7036
-          # https://github.com/gfx-rs/gfx/issues/2309
-          # https://mac.install.guide/commandlinetools/7
-          shellHook = ''
-            export PATH=/usr/bin:$PATH
-          '';
+        craneLib = (crane.mkLib pkgs).overrideToolchain (p: p.rust-bin.stable.latest.default);
+        # Common arguments can be set here to avoid repeating them later
+        # Note: changes here will rebuild all dependency crates
+        commonArgs = {
+          src = craneLib.cleanCargoSource ./.;
+          strictDeps = true;
 
-          buildInputs = with pkgs; [
-            # clang
-            # llvmPackages_17.bintools
-            # pkg-config
-          ] ++ [
-            rust-tools
-          ] ++ (with pkgs.darwin.apple_sdk.frameworks; pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            # System
-            # IOKit
-            # Security
-            # CoreFoundation
-            # AppKit
-          ]);
+          buildInputs = [ ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [ ];
         };
-      }
-    );
+
+        cript = craneLib.buildPackage (commonArgs // {
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+          # Additional environment variables or build phases/hooks can be set
+          # here *without* rebuilding all dependency crates
+          # MY_CUSTOM_VAR = "some value";
+        });
+      in {
+        checks = { inherit cript; };
+
+        packages.default = cript;
+
+        apps.default = flake-utils.lib.mkApp { drv = cript; };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            # pkgs.ripgrep
+          ];
+        };
+      });
 }
